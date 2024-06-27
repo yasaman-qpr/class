64cfb654033973a7dd7c2fac024a495312d6077d
yasaman <yasaman.qpr@gmail.com>
2024-06-27 13:56:47 +0330

    complete main

--- sql_app/main.py
+++ sql_app/main.py
@@ -22,6 +22,83 @@ def get_db():
         db.close()
 
 
+
+#دانشجو ها
+@app.post("/Creatstu/", response_model=schemas.Student)
+def create_student(student: schemas.Student, db: Session = Depends(get_db)):
+    db_student = crud.get_student(db,STid=student.STid)
+    if db_student:
+        raise HTTPException(status_code=400, detail="STid already exists")
+    return crud.create_student(db=db, student=student)
+
+
+
+@app.get("/GetStu/{student_id}", response_model=schemas.Student)
+def read_student(student_id: int, db: Session = Depends(get_db)):
+    db_student = crud.get_student(db, student_id=student_id)
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return db_student
+
+
+
+@app.put("/updatestudent/{student_id}", response_model=schemas.Student)
+def update_student(student_id: str,student:schemas.Student,db:Session = Depends(get_db)):
+    db_student = crud.update_student(db,student_id, student)
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="student not found")
+    return db_student
+
+
+
+@app.get("/delstudent/{student_id}",response_model=schemas.Student)
+def delete_student(student_id:int,db:Session=Depends(get_db)):
+    db_student = crud.get_student(db,Student_id=student_id)
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="student not found")
+    crud.removestudent(db,Student_id=student_id)
+    return db_student
+
+
+
+#استاد ها
+@app.post("/Creatostad/", response_model=schemas.Ostad)
+def create_ostad(ostad: schemas.ostad, db: Session = Depends(get_db)):
+    db_ostad = crud.get_ostad(db,Lid=ostad.Lid)
+    if db_ostad:
+        raise HTTPException(status_code=400, detail="ostad already exists")
+    return crud.create_ostad(db=db, ostad=ostad)
+
+
+
+@app.get("/Getostad/{ostad_id}", response_model=schemas.Ostad)
+def read_Ostad(ostad_id: int, db: Session = Depends(get_db)):
+    db_ostad = crud.get_ostad(db, ostad_id=ostad_id)
+    if db_ostad is None:
+        raise HTTPException(status_code=404, detail="ostad not found")
+    return db_ostad
+
+@app.put("/updateostad/{ostad_id}", response_model=schemas.Ostad)
+def update_ostad(ostad_id: str,ostad:schemas.Ostad,db:Session = Depends(get_db)):
+    db_ostad = crud.update_ostad(db,ostad_id, ostad)
+    if db_ostad is None:
+        raise HTTPException(status_code=404, detail="ostad not found")
+    return db_ostad
+
+
+
+@app.get("/delostad/{ostad_id}",response_model=schemas.Ostad)
+def delete_ostad(ostad_id:int,db:Session=Depends(get_db)):
+    db_ostad =crud.get_ostad(db,Ostad_id=ostad_id)
+    if db_ostad is None:
+        raise HTTPException(status_code=404, detail="ostad notfound")
+    crud.removeostad(db,ostad_id=ostad_id)
+    return db_ostad
+
+
+
+
+#درس ها
 @app.post("/CreatCou/", response_model=schemas.Course)
 def create_course(course: schemas.Course, db: Session = Depends(get_db)):
     db_course = crud.get_course(db,cid=course.cid)
@@ -39,5 +116,19 @@ def read_course(course_id: int, db: Session = Depends(get_db)):
     return db_course
 
 
+@app.put("/updatecourse/{course_id}", response_model=schemas.Course)
+def update_course(course_id: str,course:schemas.Course,db:Session = Depends(get_db)):
+    db_course = crud.update_course(db,course_id, course)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="course not found")
+    return db_course
 
 
+
+@app.get("/delcourse/{course_id}",response_model=schemas.Course)
+def delete_course(course_id:int,db:Session=Depends(get_db)):
+    db_course = crud.get_course(db,Course_id=course_id)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course notfound")
+    crud.removecourse(db,course_id=course_id)
+    return db_course
